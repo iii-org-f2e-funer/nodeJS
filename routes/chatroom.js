49190285cfc0fe1beea869d72424765802dd79c6
PH-LEE <leepai311@gmail.com>
2019-05-22 01:54:16 +0800

    update chatroom server v3-1
    update chatroom server v3-1

--- routes/chatroom.js
+++ routes/chatroom.js
@@ -33,7 +33,7 @@ router.get("/message/user_id1",(req,res)=>{
       //values: ['David']
     }).then(data=> {
       console.log(data);
-      res.send(data);
+      res.json(data);
       // error will be an Error if one occurred during the query
       // results will contain the results of the query
       // fields will contain information about the returned results fields (if any)
