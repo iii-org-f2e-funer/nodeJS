b95164430a11246e2ae1465f5b5a19b8bb42eec2
op30132 <op30132@gmail.com>
2019-05-21 09:49:44 +0800

    firm

--- app.js
+++ app.js
@@ -3,8 +3,22 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const multer = require('multer')
 const url = require('url')
-
+const mysql = require('mysql')
+const session = require('express-session')
 const app = express()
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'happy6'
+});
+// Error handling
+db.connect((error) => {
+    if (error) {
+        console.log('MySQL連線失敗 Error: ' + error.code)
+    }
+});
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -22,7 +36,7 @@ var corsOptions = {
 }
 app.use(cors(corsOptions))
 
-const session = require('express-session')
+
 app.use(session({
     saveUninitialized: false,
     resave: false,
@@ -30,19 +44,50 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 },  //30天
 }));
 
-app.get('/', function (req, res) {
-    res.send("hello")
-})
-app.post('/firmLogin', function (req, res) {
-    res.json({
-        message: 'success login',
-        body: req.body
+var user
+app.get('/UserInfo', function (req, res) {
+    console.log(user)
+    let sql = "SELECT * FROM `firm_manage` WHERE `account` = (?)"
+    db.query(sql, [user], (error, results, fields) => {
+        if (results[0] === undefined) {
+            res.send("no data")
+            return
+        }
+
     })
+})
+
+app.post('/firmLogin', function (req, res) {
+    const data = { success: false, message: '' }
+    data.body = req.body
+    let sql = "SELECT * FROM `firm_manage` WHERE `account` = (?)"
+    db.query(sql, [data.body.account], (error, results, fields) => {
+        if (error) throw error
+        if (results[0] === undefined) {
+            data.message = '帳號或密碼錯誤'
+            res.json({ data })
+            return
+        }
+        if (results[0].password === data.body.password) {
+            user = req.session.user = data.body.account
+            data.success = true
+            data.message = '登入成功'
+            data.user = results[0].contacter
+            res.json({ data })
+            return
+        } else {
+            data.message = '帳號或密碼錯誤'
+            res.json({ data })
+            return
+        }
+    });
+
+
 })
 
 // routes 
 const test = require('./routes/test')
-app.use('/test',test)
+app.use('/test', test)
 
 app.listen(3002, function () {
     console.log('nodeJS started on port 3002')
--- routes/test.js
+++ routes/test.js
@@ -3,17 +3,18 @@ const router = express.Router();
 const mysql = require('mysql')
 
 const db = mysql.createConnection({
-    host: '192.168.27.25',
-    user: 'happy6',
-    password: 'dj94xk46',
+    host: 'localhost',
+    user: 'root',
+    password: '',
     database: 'happy6'
 });
 // Error handling
-db.connect((error)=>{
-   if (error){
+db.connect((error) => {
+    if (error) {
         console.log('MySQL連線失敗 Error: ' + error.code)
-   }
+    }
 });
+
 
 router.get('/', (req, res) => {
     res.send("Hello")
@@ -22,9 +23,9 @@ router.post('/post', (req, res) => {
     res.send(req.body)
 });
 
-router.get('/try-db', (req, res)=>{
+router.get('/try-db', (req, res) => {
     let sql = "SELECT * FROM `member` WHERE member_id = 1"
-    db.query(sql, (error, results, fields)=>{
+    db.query(sql, (error, results, fields) => {
         res.json(results)
     });
 
