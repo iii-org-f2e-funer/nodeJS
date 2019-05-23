4b5222b295ce6c51dbc71d92743071567f8e3799
jerry905925 <jerry905925@gmail.co>
2019-05-24 00:09:04 +0800

    IG add comment

--- routes/instagram.js
+++ routes/instagram.js
@@ -27,6 +27,28 @@ router.get('/', (req, res) => {
     res.send("Hello Instagram")
 });
 
+// 撈DB貼文
+router.get('/getStories', (req, res) => {
+    // connect to MySQL DB 
+    db.connect((error) => {
+        if (error) {
+            console.log('MySQL連線失敗 Error: ' + error.code)
+        }
+    });
+    // sql stories join member
+    var sql = "SELECT `instagram_stories`.*, `member`.`nickname`,`member`.`photo` AS `avatar` FROM `instagram_stories` JOIN `member` WHERE  `instagram_stories`.`member_id` = `member`.`member_id`";
+    db.query(sql, (error, results, fields) => {
+        if (!error) {
+            for (let i = 0 ; i < results.length ; i ++){
+                //圖片 str 還原成陣列
+                results[i].photos = results[i].photos.split(',');
+            }
+            // array.reverse() 最新的在前面
+            res.json(results.reverse());
+        }
+    });
+})
+
 // 新增貼文
 router.post('/newStory', upload.array('photos'), (req, res) => {
     // req.body.memberID
@@ -51,7 +73,9 @@ router.post('/newStory', upload.array('photos'), (req, res) => {
     });
 });
 
-router.get('/getStories', (req, res) => {
+// 撈DB留言
+router.post('/getComments', (req, res) => {
+    // req.body.postID
     // connect to MySQL DB 
     db.connect((error) => {
         if (error) {
@@ -59,17 +83,37 @@ router.get('/getStories', (req, res) => {
         }
     });
     // sql stories join member
-    var sql = "SELECT `instagram_stories`.*, `member`.`nickname`,`member`.`photo` AS `avatar` FROM `instagram_stories` JOIN `member` WHERE  `instagram_stories`.`member_id` = `member`.`member_id`";
-    db.query(sql, (error, results, fields) => {
+    var sql = "SELECT `instagram_comments`.*, `member`.`nickname`,`member`.`photo` AS `avatar` FROM `instagram_comments` JOIN `member` WHERE `post_id`= ? AND `instagram_comments`.`member_id` = `member`.`member_id`";
+    db.query(sql, req.body.postID ,(error, results, fields) => {
         if (!error) {
-            for (let i = 0 ; i < results.length ; i ++){
-                //圖片 str 還原成陣列
-                results[i].photos = results[i].photos.split(',');
-            }
-            // array.reverse() 最新的在前面
-            res.json(results.reverse());
+            res.json({success:true,data:results});
         }
     });
 })
+
+// 新增留言
+router.post('/newComment',  (req, res) => {
+    // console.log(req.body)
+    // { postID: 46, userID: 1, content: 'comment Test' }
+
+    // connect to MySQL DB 
+    db.connect((error) => {
+        if (error) {
+            console.log('MySQL連線失敗 Error: ' + error.code)
+        }
+    });
+    // query
+    var sql = "INSERT INTO `instagram_comments`(`post_id`, `member_id`, `content`) VALUES (?,?,?)";
+    db.query(sql, [req.body.postID, req.body.userID, req.body.content], (error, results, fields) => {
+        if (!error) {
+            res.json({ success: true })
+        } else {
+            res.json({ success: false })
+        }
+    });
+
+});
+
+
 
 module.exports = router;
