f7df9c9a3a9b83273cb97ad2c30596e054472602
sujigary <yhybs@hotmail.com>
2019-05-25 15:28:39 +0800

    新增揪團資料頁router

--- routes/event.js
+++ routes/event.js
@@ -6,6 +6,7 @@ const db = mysql.createConnection(db_config)
 const multer = require('multer');
 const upload = multer({dest: 'image/uploads/'})
 const fs = require('fs');
+const util = require('util'); 
 
 // Error handling
 db.connect(error => {
@@ -19,6 +20,7 @@ db.connect(error => {
 router.get('/', (req, res) => {
     res.send("Hello")
 });
+
 
 //上傳揪團圖片
 router.post('/imgupload', upload.single('pt_img'),(req,res) =>{
@@ -50,8 +52,9 @@ router.post('/imgupload', upload.single('pt_img'),(req,res) =>{
     }
 })
 
+
+//新增揪團
 router.post('/newptsubmit', upload.single(),(req,res) =>{
-  console.log(req.body)
 
   let  sql = "INSERT INTO `party_manage`(`pt_host`, `pt_img`, `pt_member`, `pt_maxm`,`pt_time`, `pt_endtime`, `pt_city`, `pt_dist`, `pt_add`, `pt_title`, `pt_info`,`pt_level` ) VALUES (?,?,?,?,?,?,?,?,?,?,?,? )";
 
@@ -73,6 +76,51 @@ router.post('/newptsubmit', upload.single(),(req,res) =>{
           res.json({ success: false })
       }
     })
+});
+
+
+//ptlist paginate
+router.post('/ptlist',upload.none(),function (req, res) {  
+  let PER_PAGE = 16;
+  let offset = req.body.offset ? parseInt(req.body.offset, 10) : 0;
+  let nextOffset = offset + PER_PAGE;
+  let previousOffset = offset - PER_PAGE < 1 ? 0 : offset - PER_PAGE;
+
+  let sql = 'SELECT * FROM `party_manage` WHERE `pt_state` = 1 ORDER BY `pt_sid` desc'
+  db.query(sql, (error, results, fields) => {
+    let items = results;    
+    
+    function getPaginatedItems(items, offset) {
+      return items.slice(offset, offset + PER_PAGE);
+    }
+
+    let meta = {
+      limit: PER_PAGE,
+      next: util.format('?limit=%s&offset=%s', PER_PAGE, nextOffset),
+      offset: req.query.offset,
+      previous: util.format('?limit=%s&offset=%s', PER_PAGE, previousOffset),
+      total_count: items.length
+    };
+  
+    let json = {
+      meta: meta,
+      items: getPaginatedItems(items, offset),
+    };
+    return res.json(json)
+     })
+
+}
+)
+//讀取揪團頁面資料
+router.post('/ptinfo',function (req, res) {  
+  
+  let ptsid=req.body.ptsid;
+  let sql = 'SELECT * FROM `party_manage` WHERE `pt_sid`=(?)';
+
+  db.query(sql, [req.body.ptsid], (error, results, fields) => {
+    console.log(results)
+    res.json(results)
+  })
 });
 
 
