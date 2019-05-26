aba882ef2facf8f6753b15b0a5b07b120241fbb5
op30132 <op30132@gmail.com>
2019-05-27 01:19:24 +0800

    5/27

--- routes/firm.js
+++ routes/firm.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const mysql = require('mysql')
-const db_config = require('../datebase_config.js');
-const db = mysql.createConnection(db_config);
+const db = require('./db')
+
 
 router.get('/userInfo', function (req, res) {
   const data = { success: false, isFirm: false }
@@ -34,6 +33,7 @@ router.post('/firmLogin', function (req, res) {
     }
     if (results[0].password === data.body.password) {
       req.session.user = data.body.account
+      req.session.userSid = results[0].sid
       req.session.isFirm = true
       data.success = true
       data.message = '登入成功'
@@ -147,6 +147,79 @@ router.post('/passwordEdit', function (req, res) {
       data.success = true
       data.body = req.body
       data.message = '帳號資料修改成功'
+      res.json(data)
+    } else {
+      data.message = '修改失敗'
+      res.json(data)
+    }
+  })
+})
+
+//廠商資料設定
+router.get('/firmInfo', function (req, res) {
+  const data = { success: false, message: '' }
+  let sql = 'SELECT * FROM `site_manage` WHERE `firm_id` = (?)'
+  db.query(sql, [req.session.userSid], (error, results, fields) => {
+    if (results[0] === undefined) {
+      data.message = '此店家無場地資料'
+      res.json(data)
+    } else {
+      data.body = results[0]
+      db.query('SELECT * FROM `site_image` WHERE `site_id` = (?)', [results[0].sid], (error, img_results, fields) => {
+        if (img_results[0] === undefined) {
+          data.success = true
+          data.message = '此店家提供場地，暫無照片'
+        } else {
+          data.success = true
+          data.message = '獲得場地資料及照片'
+          data.img = img_results
+        }
+        res.json(data)
+      })
+    }
+  })
+})
+//新增
+router.get('/insertAccount', function (req, res) {
+  const data = { success: false, message: '' }
+  let sql = 'INSERT INTO `site_manage` (sid,firm_id,store,county,dist,address,phone,business_hours,public_holiday,charges,about,rule,status) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)'
+  db.query(sql, [null, req.body.firm_id, req.body.store, req.body.county, req.body.dist, req.body.address, req.body.phone, req.body.business_hours, req.body.public_holiday, req.body.charges, req.body.about, req.body.rule, req.body.status], (error, results, fields) => {
+    if (error) throw error
+    if (results.affectedRows === 1) {
+      data.success = true
+      data.message = '店家資料新增成功'
+      data.body = req.body
+      res.json(data)
+    } else {
+      data.message = '註冊失敗'
+      res.json(data)
+    }
+  })
+})
+//更新
+router.get('/updateAccount', function (req, res) {
+  const data = { success: false, message: '' }
+  let sql = 'UPDATE `site_manage` SET ? WHERE `sid` = ?'
+  db.query(sql, [{
+    sid: req.body.sid,
+    firm_id: req.body.sid,
+    store: req.body.store,
+    county: req.body.county,
+    dist: req.body.dist,
+    address: req.body.address,
+    phone: req.body.phone,
+    business_hours: req.body.business_hours,
+    public_holiday: req.body.public_holiday,
+    charges: req.body.charges,
+    about: req.body.about,
+    rule: req.body.rule,
+    status: req.body.status,
+  }, req.body.sid], (error, results, fields) => {
+    if (error) throw error
+    if (results.affectedRows === 1) {
+      data.success = true
+      data.body = req.body
+      data.message = '店家資料修改成功'
       res.json(data)
     } else {
       data.message = '修改失敗'
