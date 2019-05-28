b6807fbbeb434679f9a669f1fbee94a6c56dfde3
jerry905925 <jerry905925@example.com>
2019-05-28 12:54:25 +0800

    db fixed

--- routes/db.js
+++ routes/db.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: '192.168.27.25',
+    host: 'localhost',
     user: 'happy6',
     password: 'dj94xk46',
     database: 'happy6'
