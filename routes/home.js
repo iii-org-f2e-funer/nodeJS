a9ae40c7cdecd733f1528872f6a70c329b0efaf8
jerry905925 <jerry905925@gmail.co>
2019-05-30 02:35:24 +0800

    v2

--- routes/home.js
+++ routes/home.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
 
 router.get('/homeEvent', (req, res) => {
     // query
-    var sql = "SELECT `pt_sid`, `pt_title`, `pt_img`, `pt_time`, `pt_dist` FROM `party_manage` WHERE 1  ORDER BY `pt_sid` DESC LIMIT 10";
+    var sql = "SELECT `pt_sid`, `pt_title`, `pt_img`, `pt_time`, CONCAT(`pt_dist`,', ',`pt_add`) as `pt_add` FROM `party_manage` WHERE 1  ORDER BY `pt_sid` DESC LIMIT 10";
     db.query(sql, [req.body.userID, req.body.postID], (error, results, fields) => {
         if (!error) {
             for (let i = 0 ; i<results.length;i++){
@@ -25,4 +25,16 @@ router.get('/homeEvent', (req, res) => {
     });
 });
 
+router.get('/homeProduct', (req, res) => {
+    // query
+    var sql = "SELECT `product_manage`.`sid`, `productName`, `price`,`product_manage_images`.`image_path` FROM `product_manage` JOIN `product_manage_images` WHERE `product_manage`.`sid` = `product_manage_images`.`product_id` GROUP BY `product_manage`.`sid`";
+    db.query(sql, [req.body.userID, req.body.postID], (error, results, fields) => {
+        if (!error) {
+            res.json({ success: true, data: results })
+        } else {
+            res.json({ success: false })
+        }
+
+    });
+});
 module.exports = router;
